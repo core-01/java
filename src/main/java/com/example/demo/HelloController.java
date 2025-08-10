783ab05020fb314c5288aeab121456ba07a22cb0
Raghvendra Singh <core.raghvendra@gmail.com>
2025-08-10 18:26:01 +0000

    Test 1

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -6,13 +6,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    private int counter = 0;
+
     @GetMapping("/")
     public String home() {
-        return "Spring Boot is running in Codespaces!";
+        return "Welcome! Try /hello to see the counter.";
     }
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from Spring Boot in Codespaces!";
+        counter++;
+        String message = "Hello from Spring Boot in Codespaces! This endpoint has been called " + counter + " times.";
+
+        if (counter > 5) {
+            message += " 🤪 Okay, now you're just showing off!";
+        }
+
+        return message;
     }
 }
